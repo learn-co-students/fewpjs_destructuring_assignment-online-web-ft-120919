fa068ece4e12c318bb8e7c8130fc9be2cfe59193
github username <meliajm@gmail.com>
2020-04-01 09:32:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const farmAnimals = 'cow horse sheep pig chicken'
-
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const [Bessie, , Dolly, Babe, Little] = farmAnimals.split(' ')
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+const [r, o, y, g, b, , v] = colors
+const [, , , , , indg, ] = colors
+
+const [black_and_white, , black, pink, ] = farmAnimals.split(' ')
+
+
+
 
 const muppet = {
   name: 'Kermit',
@@ -9,6 +20,7 @@ const muppet = {
   job: 'Host of The Muppet Show',
   partner: 'Miss Piggy'
 };
+
 
 const k_muppet = {
   k_name: 'Kermit',
@@ -25,17 +37,15 @@ const k_muppet = {
   k_partner: 'Miss Piggy'
 };
 
-// Strings
+const { name, color, song, job, partner } = muppet
 
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
+const { k_name, k_color, k_job, k_partner } = k_muppet
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
+const { song_1, song_2, song_3, song_4} = k_muppet.k_album.the_muppet_movie
 
-// Arrays
 
-// 4. Use destructuring to assign appropriate variables using the color names.
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
@@ -46,4 +56,3 @@ const k_muppet = {
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
 
 
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
